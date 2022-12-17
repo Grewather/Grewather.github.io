d37f80a5a113121721125331ba1811d2c87afdaa
Grewather <69985703+Grewather@users.noreply.github.com>
2022-12-17 19:30:32 +0100

    All project

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+let typingNick = document.querySelector(".typingEffectNick");
+var typewriter = new Typewriter(typingNick, {
+  loop: true,
+});
+
+typewriter
+  .start()
+  .pauseFor(1000)
+  .typeString("Grewather!")
+  .pauseFor(1000)
+  .deleteAll()
+  .typeString("Programmer!")
+  .pauseFor(1000)
+  .deleteAll()
+  .typeString("Webdeveloper!");
